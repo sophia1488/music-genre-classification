4ef5c949795eb3369eb0a3320563e0e843d9858d
Sophia Chou <sophia14tw315@gmail.com>
2021-09-13 22:35:33 +0800

    update

---
+++ genre-classification/dataset.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset, DataLoader
+import pickle
+import torch
+
+class AudioDataset(Dataset):
+    def __init__(self, pkl_dir, transforms=None):
+        self.data = []
+        self.length = 1500 
+        self.transforms = transforms
+        with open(pkl_dir, "rb") as f:
+            self.data = pickle.load(f)
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        entry = self.data[idx]
+        output_data = {}
+        values = entry["values"].reshape(-1, 128, self.length)
+        values = torch.Tensor(values)
+        if self.transforms:
+            values = self.transforms(values)
+        target = torch.LongTensor([entry["target"]])
+        return (values, target)
---
+++ genre-classification/utils.py
@@ -0,0 +1,43 @@
+import os
+import matplotlib.pyplot as plt
+import itertools
+import shutil
+import torch
+import numpy as np
+
+
+def save_checkpoint(state, is_best, checkpoint_dir):
+    filename = os.path.join(checkpoint_dir, 'model.ckpt')
+    if not os.path.exists(checkpoint_dir):
+        print("Checkpoint Directory does not exist")
+        os.mkdir(checkpoint_dir)
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, os.path.join(checkpoint_dir, "model_best.ckpt"))
+  
+
+def save_cm_fig(cm, classes, normalize, title, save_dir):
+    if normalize:
+        cm = cm.astype('float')*100/cm.sum(axis=1)[:,None]
+   
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title, fontsize=16)
+#    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks([])
+#    plt.yticks([])
+#    plt.xticks(tick_marks, classes, fontsize=)
+    plt.yticks(tick_marks, classes, fontsize=12)
+
+    fmt = '.2f' if normalize else 'd'
+    threshold = cm.max()/2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i,j],fmt), fontsize=9,
+                horizontalalignment='center',
+                color='white' if cm[i,j] > threshold else 'black')
+    plt.xlabel('predicted', fontsize=14)
+    plt.ylabel('true', fontsize=14)
+    plt.tight_layout()
+    
+    plt.savefig(f'{save_dir}/cm_{title}.jpg')
+    return
