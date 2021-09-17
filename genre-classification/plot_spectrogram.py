d878dc7ea9a60fee1b176068a9fa249bd8ffbd5a
Sophia Y. Chou <47291963+sophia1488@users.noreply.github.com>
2021-09-17 10:59:58 +0800

    Update plot_spectrogram.py

--- genre-classification/plot_spectrogram.py
+++ genre-classification/plot_spectrogram.py
@@ -1,5 +1,30 @@
 from matplotlib import pyplot as plt
+import librosa
+import numpy as np
+import torchaudio
+import torch
 
+
+def extract_spectrogram(sr, clip):
+	num_channels = 3
+	window_sizes = [25, 50, 100]
+	hop_sizes = [10, 25, 50]
+
+	specs = []
+	for i in range(num_channels):
+		window_length = int(round(window_sizes[i]*sr/1000))
+		hop_length = int(round(hop_sizes[i]*sr/1000))
+
+		clip = torch.Tensor(clip)
+		spec = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_fft=2205, win_length=window_length, hop_length=hop_length, n_mels=128)(clip) #Check this otherwise use 2400
+		eps = 1e-6
+		spec = spec.numpy()
+		spec = np.log(spec+ eps)
+		# Resize is scaling, not crop
+		spec = np.asarray(torchvision.transforms.Resize((128, 1500))(Image.fromarray(spec)))
+		return spec
+    
+    
 sampling_rate = 22050
 f, axs = plt.subplots(5,2,figsize=(15,15))
 
@@ -8,7 +33,7 @@ MAP = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop
 for i, genre in enumerate(MAP):
     audio = f'../Data/genres_original/{genre}/{genre}.00000.wav'
     clip, sr = librosa.load(audio, sr=sampling_rate)
-    spec = extract_spectrogram(sampling_rate, [], clip, -1)
+    spec = extract_spectrogram(sampling_rate, clip)
     spec = spec[:, 500:800]   # randomly crop it
     
     r, c = i // 2, i % 2
