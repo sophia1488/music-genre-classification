f900f665a712520f76f8f3e3e22718fa805fcbf3
Sophia Y. Chou <47291963+sophia1488@users.noreply.github.com>
2021-09-17 11:02:11 +0800

    Update plot_spectrogram.py

--- genre-classification/plot_spectrogram.py
+++ genre-classification/plot_spectrogram.py
@@ -6,25 +6,25 @@ import torchaudio
 
 
 def extract_spectrogram(sr, clip):
-	num_channels = 3
-	window_sizes = [25, 50, 100]
-	hop_sizes = [10, 25, 50]
+    num_channels = 3
+    window_sizes = [25, 50, 100]
+    hop_sizes = [10, 25, 50]
 
-	specs = []
-	for i in range(num_channels):
-		window_length = int(round(window_sizes[i]*sr/1000))
-		hop_length = int(round(hop_sizes[i]*sr/1000))
+    specs = []
+    for i in range(num_channels):
+        window_length = int(round(window_sizes[i]*sr/1000))
+        hop_length = int(round(hop_sizes[i]*sr/1000))
 
-		clip = torch.Tensor(clip)
-		spec = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_fft=2205, win_length=window_length, hop_length=hop_length, n_mels=128)(clip) #Check this otherwise use 2400
-		eps = 1e-6
-		spec = spec.numpy()
-		spec = np.log(spec+ eps)
-		# Resize is scaling, not crop
-		spec = np.asarray(torchvision.transforms.Resize((128, 1500))(Image.fromarray(spec)))
-		return spec
+        clip = torch.Tensor(clip)
+        spec = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_fft=2205, win_length=window_length, hop_length=hop_length, n_mels=128)(clip) #Check this otherwise use 2400
+        eps = 1e-6
+        spec = spec.numpy()
+        spec = np.log(spec+ eps)
+        # Resize is scaling, not crop
+        spec = np.asarray(torchvision.transforms.Resize((128, 1500))(Image.fromarray(spec)))
+        return spec
     
-    
+
 sampling_rate = 22050
 f, axs = plt.subplots(5,2,figsize=(15,15))
 
